a5aae0ca304c18f04964160aa989d63bfdf77b58
Dan <djenkinsjg0617@gmail.com>
2026-03-03 20:10:17 -0600

    Added shell variables

--- sync.sh
+++ sync.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-LOGFILE=""
+LOGFILE=$2
 
-cd .
+SYNC_DIR=$1
+
+cd $SYNC_DIR
 
 
 
 while inotifywait -qr --event modify . >> "$LOGFILE" 2>&1; do 
+	
 	sleep 30;
 	
-	cd .
-
-
 	echo "--- Sync started at $(date) ---" >> "$LOGFILE"
 
+	git pull >> "$LOGFILE" 2>&1
 	git add . >> "$LOGFILE" 2>&1
 	git commit -m "sync" >> "$LOGFILE" 2>&1
 	git push origin main >> "$LOGFILE" 2>&1
 
 	echo "--- Sync complete ---" >> "$LOGFILE"
-
 
 done 
